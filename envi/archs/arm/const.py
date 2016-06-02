76b511aa1b30bc660270e0b53c3ced47e66915e0
redheadedrod <redheadedrod@hotmail.com>
2016-06-02 14:10:56 -0400

    6/2 merge with latest code from atlas and updated unit tests

--- envi/archs/arm/const.py
+++ envi/archs/arm/const.py
@@ -3,6 +3,32 @@ MODE_THUMB      = 1
 MODE_JAZELLE    = 2
 MODE_THUMBEE    = 3
 
+'''
+bitfields to support different versions
+For simplicity will use 0xFFFF to say all.
+Note that thumb and arm are both represented
+thumbee is shown but may never be fully implimented
+since was already depreciated
+'''
+archBitMask = [
+#    architecture   bitmask              dec     hex
+    ('THUMB16',     0b0000000000000001), # 1      1
+    ('THUMB2',      0b0000000000000010), # 2      2
+    ('THUMBEE',     0b0000000000000100), # 4      4
+    ('reserved',    0b0000000000001000), # 8      8    reserved for future usage
+    ('ARMv4',       0b0000000000010000), # 16     10
+    ('ARMv5',       0b0000000000100000), # 32     20
+    ('ARMv5T',      0b0000000001000000), # 64     40
+    ('ARMv5E',      0b0000000010000000), # 128    80
+    ('ARMv5J',      0b0000000100000000), # 256    100
+    ('ARMv6',       0b0000001000000000), # 512    200
+    ('ARMv6T2',     0b0000010000000000), # 1024   400
+    ('ARMv7M',      0b0000100000000000), # 2048   800 
+    ('ARMv7AR',     0b0001000000000000), # 4096   1000
+    ('ARMv8',       0b0010000000000000), # 8192   2000
+]
+number_of_archs = len(archBitMask)
+
 #IFLAGS - keep bottom 8-bits for cross-platform flags like envi.IF_NOFALL and envi.IF_BRFALL
 IF_PSR_S     = 1<<32     # This DP instruciton can update CPSR
 IF_B         = 1<<33     # Byte
@@ -13,6 +39,7 @@ IF_L         = 1<<38    # Long-store (eg. Dblword Precision) for STC
 IF_T         = 1<<39    # Translate for strCCbt
 IF_W         = 1<<40    # Write Back for STM/LDM (!)
 IF_UM        = 1<<41    # User Mode Registers for STM/LDM (^) (obviously no R15)
+IF_TT        = 1<<42    # Flag for STRT/LDRT and STRBT/LDRBT etc
 
 IF_DAIB_SHFT = 56       # shift-bits to get DAIB bits down to 0.  this chops off the "is DAIB present" bit that the following store.
 IF_DAIB_MASK = 7<<(IF_DAIB_SHFT-1)
