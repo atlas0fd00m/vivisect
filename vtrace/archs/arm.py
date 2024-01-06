a8558c0597d614d4d7f67856c0956d64d8955890
atlas <atlas@r4780y.com>
2024-01-06 16:57:09 -0500

    making ARM ready

--- vtrace/archs/arm.py
+++ vtrace/archs/arm.py
@@ -6,7 +6,10 @@ class ArmMixin(e_arm.ArmModule, e_arm.ArmRegisterContext):
         e_arm.ArmModule.__init__(self)
         e_arm.ArmRegisterContext.__init__(self)
 
+        self.setMeta('Architecture', 'arm')
+
     def archGetStackTrace(self):
+        self.requireAttached()
         pc = self.getProgramCounter()
         sp = self.getStackCounter()
         return [ (pc,sp), ]
